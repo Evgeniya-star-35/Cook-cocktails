a5b94d8e60c7ff94ad50c71efbd8481248acca8a
Evgeniya Bayda <baje75128@gmail.com>
2021-08-24 18:48:07 +0300

    add pnotify

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 // import './sass/main.scss';
 import refs from './js/refs';
 const axios = require('axios');
+import { notice } from '@pnotify/core';
+import { error } from '@pnotify/core';
+import '@pnotify/core/dist/BrightTheme.css';
+import '@pnotify/core/dist/PNotify.css';
 
 let searchValue = '';
 function onSearchCocktail(e) {
   e.preventDefault();
   searchValue = refs.input.value;
-  //   console.log(refs.input.value);
+  
   axios
     .get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchValue}`)
     .then(result => {
       refs.gallery.innerHTML = "";
       console.log(result.data.drinks);
+      if (result.data.drinks === null) {
+        noFound();
+      }
       renderGallery(result.data.drinks);  
       refs.form.reset();
     })
@@ -20,14 +27,19 @@ function onSearchCocktail(e) {
 refs.form.addEventListener('submit', onSearchCocktail);
 
 function markUpItem({ strDrink, strDrinkThumb, strInstructions }) {
-  const article = `<li class='gallery-item'><img class='gallery-img' src='${strDrinkThumb}' alt='${strDrink}'/><h1 class ="gallery-title">${strDrink}</h1><p class ="gallery-text">${strInstructions}</p></li>`;
+  const article = `<li class='gallery-item'><a href = ''><img class='gallery-img' src='${strDrinkThumb}' alt='${strDrink}'/></a><h1 class ="gallery-title">${strDrink}</h1><p class ="gallery-text">${strInstructions}</p></li>`;
   refs.gallery.insertAdjacentHTML('beforeend', article);
 }
 
 function renderGallery(arr) {
   arr.forEach(el => markUpItem(el));
 }
-
+function noFound() {
+  error({
+    text: 'No matches found. Please enter another query!',
+    delay: 2500,
+  });
+}
 // async function onFetchCocktails(searchValue) {
 //   try {
 //     const responseUrl = await fetch(
